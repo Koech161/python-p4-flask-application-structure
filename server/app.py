766aa6cbda37d33e0b99de40f5a6ca79598e7089
Abel Koech <koechabel161@gmail.com>
2024-09-30 16:13:10 +0300

    flask app

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:abel>')
+def user(abel):
+    return f'<h1>Profile for {abel}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
